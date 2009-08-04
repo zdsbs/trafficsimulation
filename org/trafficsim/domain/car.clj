bcee19ba4664f6292d4bd1c2edfda9c840f4501d
Daniel Pierkowski <dpierkowski@gmail.com>
2009-08-03 21:41:06 -0400

    add driver behavior structure and speed changes based on same

--- org/trafficsim/domain/car.clj
+++ org/trafficsim/domain/car.clj
@@ -1,23 +1,23 @@
 (ns org.trafficsim.domain.car)
 
-(defstruct car :length :speed :position)
+(def IN-FRONT (. Integer MAX_VALUE))
+
+(defstruct behavior :accel-dist :decel-dist :target-speed)
+(defstruct car :length :speed :position :acc-behavior :braking-behavior :max-speed-behavior)
 
 (defn brake [dec name cur-speed]
 	[name (- cur-speed dec)]
 )
 
-(defn granny-speed [name speed distance]
-	(if (and (not (nil? distance)) (< distance 4))
-	  (brake 1 name speed)
-	  [name speed]
-	)
-)
+(defn accel [acc name cur-speed]
+	[name (+ cur-speed acc)])
 
-(defn apply-speed-function [car-with-speed-function]
-	(apply (last car-with-speed-function) (butlast car-with-speed-function))
-)
+(defn speed-change [[behavior name speed distance]]
+	(cond 
+		(< distance (:decel-dist behavior)) (brake 1 name speed)
+		(> distance (:accel-dist behavior)) (accel 1 name speed)
+		:else [name speed]
+		))
 
-(defn get-all-actions [cars-with-speed-functions]
-	(map apply-speed-function cars-with-speed-functions)
-)
-
+(defn collect-all-new-speed-changes [braking-data] 
+	(map speed-change braking-data))
--- org/trafficsim/domain/test/car_test.clj
+++ org/trafficsim/domain/test/car_test.clj
@@ -5,25 +5,19 @@
 	(is (= [:a 1] (brake 1 :a 2)))
 )
 
-(deftest granny-speed-test
-	(is (= [:a 0] (granny-speed :a 1 3)))
-	(is (= [:a 1] (granny-speed :a 1 4)))
-	(is (= [:a 1] (granny-speed :a 1 5)))
-	(is (= [:a 1] (granny-speed :a 1 nil)))
-	)
 
-(deftest apply-speed-function-test
-	(is (= [:a 0] (apply-speed-function [:a 1 3 granny-speed])))
-	(is (= [:a 1] (apply-speed-function [:a 1 nil granny-speed])))
-)
+(deftest speed-change-test
+	(is (= [:a 4] (speed-change [{:decel-dist 2 :accel-dist 10} :a 5 1])))
+	(is (= [:a 5] (speed-change [{:decel-dist 2 :accel-dist 10} :a 5 5])))
+	(is (= [:a 6] (speed-change [{:decel-dist 2 :accel-dist 10} :a 5 11]))))
 
-(deftest get-all-actions-test
-	(is (= [[:a 4] [:b 3] [:c 4]] 
-		(get-all-actions [[:a 4 6 granny-speed] [:b 4 3 granny-speed] [:c 4 nil granny-speed]])))
-	(is (= [] (get-all-actions []))))
-
-
-
+(deftest collect-all-new-speed-changes-test
+	(is (= [[:a 4] [:b 3] [:c 5]] 
+		(collect-all-new-speed-changes 
+			[[{:decel-dist 5 :accel-dist 7} :a 4 6] 
+			[{:decel-dist 4 :accel-dist 5} :b 4 3] 
+			[{:decel-dist 2 :accel-dist 10} :c 4 IN-FRONT]])))
+	(is (= [] (collect-all-new-speed-changes []))))
 
 
 ;some pseudo-code and design thoughts....
