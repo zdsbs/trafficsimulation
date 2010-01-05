cbe88f78afeaa5751891f9c258c505842f95f478
Daniel Pierkowski <dpierkowski@gmail.com>
2010-01-04 21:37:09 -0500

    finish road-space generation

--- org/trafficsim/domain/road.clj
+++ org/trafficsim/domain/road.clj
@@ -1,46 +1,24 @@
-(ns org.trafficsim.domain.road)
+(ns org.trafficsim.domain.road
+	(:use clojure.contrib.combinatorics clojure.walk)
+)
 
 ;next week (this week) we will write a drivable space generator
 ;(drivables_space 100 10)
 
-;(def stuff [
-;[1 1 1]
-;[1 0 1]
-;])
-
-(defn cartesian-product
-  "All the ways to take one item from each sequence"
-  [& seqs]
-  (let [v-original-seqs (vec seqs)
-	step
-	(fn step [v-seqs]
-	(let [increment
-	(fn [v-seqs]
-	(loop [i (dec (count v-seqs)), v-seqs v-seqs]
-	(if (= i -1) nil
-	(if-let [rst (next (v-seqs i))]
-	(assoc v-seqs i rst)
-	(recur (dec i) (assoc v-seqs i (v-original-seqs i)))))))]
-	(when v-seqs
-	(cons (map first v-seqs)
-	(lazy-seq (step (increment v-seqs)))))))]
-	    (when (every? first seqs)
-	      (lazy-seq (step v-original-seqs)))))
+(defn space-size [num-roads road-width]
+	(- (* 2 road-width num-roads) road-width))
 
 (defn all-space [num-roads road-width]
-	(let [space-size (fn space-size [num-roads road-width] (- (* 2 road-width num-roads) road-width))]
-			(let [size (space-size num-roads road-width)]
-				(cartesian-product (range 0 size) (range 0 size)))))
+	(let [size (space-size num-roads road-width)]
+		(cartesian-product (range 0 size) (range 0 size))))
 
 (defn is-road-offset [road-width offset]
 		(mod (int (/ (+ road-width offset) road-width)) 2)) 
 
 (defn is-road-coordinate [road-width coord-seq]
-	(or 
+	(bit-or 
 		(is-road-offset road-width (first coord-seq)) 
 		(is-road-offset road-width (second coord-seq))))
-
-
 
 ;1,000 pixels of road
 (def LENGTH 1000)
@@ -48,3 +26,23 @@
 ;right now roads only have length b/c we're limiting ourselves
 ;to one-lane roads.  In the future it might have other quantities
 
+(defn no-op-formatter [coord road-width]
+	coord)
+	
+(defn actual-formatter [coord road-width]
+	(is-road-coordinate road-width (seq coord)))
+
+(defn space-row [num-roads road-width row-num formatter]
+	(loop [col-num 0 max-col (space-size num-roads road-width) row []]
+		(if (= col-num (dec max-col))
+			(conj row (formatter [row-num col-num] road-width))
+			(recur (inc col-num) max-col (conj row (formatter [row-num col-num] road-width))))))
+
+(defn space [num-roads road-width formatter]
+	(loop [row-num 0 max-row (space-size num-roads road-width) space []]
+		(if (= row-num (dec max-row))
+			(conj space (space-row num-roads road-width row-num formatter))
+			(recur (inc row-num) max-row (conj space (space-row num-roads road-width row-num formatter))))))	
+
+(defn print-space [space] 
+	(map prn space))
--- org/trafficsim/domain/test/road_test.clj
+++ org/trafficsim/domain/test/road_test.clj
@@ -5,4 +5,19 @@
 	(is (= 1 (is-road-coordinate 2 (seq [0 0]))))
 	(is (= 0 (is-road-coordinate 2 (seq [2 3]))))
 	(is (= 1 (is-road-coordinate 2 (seq [0 1]))))
-	(is (= 1 (is-road-coordinate 2 (seq [8 6])))))
+	(is (= 1 (is-road-coordinate 2 (seq [8 6]))))
+	(is (= 1 (is-road-coordinate 2 (seq [3 0])))))
+	
+
+(deftest space-test
+	(is (= (space 3 2 actual-formatter) 
+		[[1 1 1 1 1 1 1 1 1 1]
+		[1 1 1 1 1 1 1 1 1 1]
+		[1 1 0 0 1 1 0 0 1 1]
+		[1 1 0 0 1 1 0 0 1 1]
+		[1 1 1 1 1 1 1 1 1 1]
+		[1 1 1 1 1 1 1 1 1 1]
+		[1 1 0 0 1 1 0 0 1 1]
+		[1 1 0 0 1 1 0 0 1 1]
+		[1 1 1 1 1 1 1 1 1 1]
+		[1 1 1 1 1 1 1 1 1 1]])))
