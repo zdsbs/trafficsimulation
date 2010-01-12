3ebdb78a6d4986a1407f183f2cad24a51c1c842f
Daniel Pierkowski <dpierkowski@gmail.com>
2010-01-11 21:36:03 -0500

    0.1 version of traffic sim.  we're dropping this to start working on CROWD

--- org/trafficsim/domain/road.clj
+++ org/trafficsim/domain/road.clj
@@ -5,41 +5,31 @@
 (defn space-size [num-roads road-width]
 	(- (* 2 road-width num-roads) road-width))
 
-(defn all-space [num-roads road-width]
-	(let [size (space-size num-roads road-width)]
-		(cartesian-product (range 0 size) (range 0 size))))
+(defn is-coordinate-on-road? [road-width coordinate]
+		(mod (int (/ (+ road-width coordinate) road-width)) 2)) 
 
-(defn is-road-offset [road-width offset]
-		(mod (int (/ (+ road-width offset) road-width)) 2)) 
-
-(defn is-road-coordinate [road-width coord-seq]
+(defn is-point-on-road? [road-width point]
 	(bit-or 
-		(is-road-offset road-width (first coord-seq)) 
-		(is-road-offset road-width (second coord-seq))))
+		(is-coordinate-on-road? road-width (first point)) 
+		(is-coordinate-on-road? road-width (second point))))
 
-;1,000 pixels of road
-;(def LENGTH 1000)
-
-;right now roads only have length b/c we're limiting ourselves
-;to one-lane roads.  In the future it might have other quantities
-
-(defn no-op-formatter [coord road-width]
-	coord)
+(defn no-op-formatter [point road-width]
+	point)
 	
-(defn actual-formatter [coord road-width]
-	(is-road-coordinate road-width (seq coord)))
+(defn binary-formatter [point road-width]
+	(is-point-on-road? road-width (seq point)))
 
-(defn space-row [num-roads road-width row-num formatter]
+(defn space-row [num-roads road-width row-num point-formatter]
 	(loop [col-num 0 max-col (space-size num-roads road-width) row []]
 		(if (= col-num (dec max-col))
-			(conj row (formatter [row-num col-num] road-width))
-			(recur (inc col-num) max-col (conj row (formatter [row-num col-num] road-width))))))
+			(conj row (point-formatter [row-num col-num] road-width))
+			(recur (inc col-num) max-col (conj row (point-formatter [row-num col-num] road-width))))))
 
-(defn space [num-roads road-width formatter]
+(defn space [num-roads road-width point-formatter]
 	(loop [row-num 0 max-row (space-size num-roads road-width) space []]
 		(if (= row-num (dec max-row))
-			(conj space (space-row num-roads road-width row-num formatter))
-			(recur (inc row-num) max-row (conj space (space-row num-roads road-width row-num formatter))))))	
+			(conj space (space-row num-roads road-width row-num point-formatter))
+			(recur (inc row-num) max-row (conj space (space-row num-roads road-width row-num point-formatter))))))	
 
 (defn print-space [space] 
 	(map prn space))
